425011106d528c39db28186174d763e3b8506508
bayeng <88835193+bayeng@users.noreply.github.com>
2025-07-20 13:30:51 +0700

    Update AlamatController.php

--- app/Controllers/Api/AlamatController.php
+++ app/Controllers/Api/AlamatController.php
@@ -127,7 +127,7 @@ class AlamatController extends BaseController
                 'alamat' => $newAlamat
             ])->setStatusCode(ResponseInterface::HTTP_OK);
         } catch (\Exception $e) {
-            error_log($e);
+            dd($e);
             return $this->response->setJSON([
                 'error' => $e->getMessage()
             ])->setStatusCode(500);
